3af20b6b4f8225b00e3c76a6c8488bd7694fd3dd
Diego Melo <140443998+DiegoHenriqueMelo@users.noreply.github.com>
2023-10-17 16:17:19 -0300

    Adicionei conteudo

---
+++ JORNADA DO DEV/Aulas-JavaScript/ConceitosJS/ambiente.js
@@ -0,0 +1,32 @@
+//  COMO DECLARAR UMA VARIAVEL
+// CHAMADA (LET) NOME DA VARIAVEL (VARIAVEL) RECEBE O VALOR (=) VALOR(9)
+let variavel = 9
+
+// STRING
+// VALORES COLOCADOR ENTRE ASPAS 'SIMPLES' OU "DUPLAS"
+let nome = 'Diego'
+
+// NUMBER
+// INTEGERS/ NUMEROS INTEIROS
+let idade = 18
+
+// FLOATS/ NUMEROS QUEBRADOS
+let contaDoBanco = 765.90
+
+// BOLEAN/ TRUE OR FALSE
+let diegoCabeloMarrom = true
+
+// ARRAYS/ CONTEM VARIOS VALORES
+let frutas = ['Maça', 'Pêra', 'Banana']
+// COMO ACESSAR OS ITENS DA ARRAYS
+frutas[0]
+
+// OBJETOS
+// PROPRIEDADES DO OBJETO DENTRO DAS {CHAVES}
+let xicara = { cor: 'branco', material: 'porcelana', peso: 100}
+
+// ACESSANDO AS PROPRIEDADES
+xicara.material
+
+
+
