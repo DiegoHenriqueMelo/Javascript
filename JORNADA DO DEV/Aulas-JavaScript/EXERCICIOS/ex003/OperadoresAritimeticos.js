1027d03125af4bb5763e1c0a99c291eaec52b1e7
Diego Melo <140443998+DiegoHenriqueMelo@users.noreply.github.com>
2023-10-23 16:32:41 -0300

    Realizei mais exercicios

---
+++ JORNADA DO DEV/Aulas-JavaScript/EXERCICIOS/ex003/OperadoresAritimeticos.js
@@ -0,0 +1,10 @@
+// PEDRO TEM 2 CAIXAS DE PIRULITOS. COPM 20 UNIDADES CADA, E PRECISA DIVIDIR COM 5 AMIGOS. COM QUANTOS PIRULITOS CADA AMIGO FICOU?
+
+let caixaPirulito = 2
+let pirulito = 20
+let amigos = 5
+
+let totalPirulito = (caixaPirulito * pirulito) / amigos
+let total = `Cada amigo de Pedro ficou com ${totalPirulito} pirulitos`
+
+console.log(total)
---
+++ JORNADA DO DEV/Aulas-JavaScript/Operadores Aritimeticos/ambiente.js
@@ -0,0 +1,24 @@
+// SOMA
+
+10 + 10
+
+// SUBITRAÇÃO
+
+10 - 10
+
+// MULTIPLICAÇÃO
+
+10*10
+
+// DIVISÃO
+
+10/10
+
+// COMO REALIZAE AS CONTAS
+
+let num1 = 32
+let num2 = 9
+let resultado = num1 - num2
+let resultadoEscrito = `O resultado de ${num1} - ${num2} é de ${resultado}`
+
+console.log(resultadoEscrito)
