0ccf44c88c2f7d651eeda6d7c43313f319423d25
Diego Melo <140443998+DiegoHenriqueMelo@users.noreply.github.com>
2023-10-23 16:56:02 -0300

    Realizei mais um exercicio

---
+++ JORNADA DO DEV/Aulas-JavaScript/EXERCICIOS/ex004/OperadoresComparação.js
@@ -0,0 +1,8 @@
+// CRIAR UMA FORMA DE ORGANIZAR MÉDIA DOS ALUNOS DE UMA ESCOLA
+
+let nota1 = Number(prompt('Digite sua nota aqui!'))
+let nota2 = Number(prompt('Digite sua nota aqui!'))
+
+let resultado = (nota1 + nota2) / 2
+
+alert(`A média da sua nota foi de: ${resultado}`)
