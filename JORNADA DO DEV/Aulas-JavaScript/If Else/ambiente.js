39729451aec0fa54636c28a7d37b860120869db9
Diego Melo <140443998+DiegoHenriqueMelo@users.noreply.github.com>
2023-11-07 14:29:12 -0300

    Realizei novo exercicio

---
+++ JORNADA DO DEV/Aulas-JavaScript/If Else/ambiente.js
@@ -0,0 +1,15 @@
+
+let nota1 = Number(prompt('Digite sua nota aqui!'))
+let nota2 = Number(prompt('Digite sua nota aqui!'))
+
+let resultado = (nota1 + nota2) / 2
+
+
+// SE (IF) A VARIAVEL FOR MAIOR OU IGUAL (>=) 
+// SE NÃO 
+
+if(resultado >=5){
+    alert("Você foi arpovado! Parabens pelo resoltado")
+}else{
+    alert('Voce foi reprovado!')
+}
